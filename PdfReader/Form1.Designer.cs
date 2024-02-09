4fa74116019ff507808e342c32418624972ff2e3
apachaihop <poizen211@gmail.com>
2024-02-09 12:11:40 +0300

    Добавьте файлы проекта.

---
+++ PdfReader/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace PdfReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBoxArray1 = new Microsoft.VisualBasic.Compatibility.VB6.RichTextBoxArray(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.richTextBoxArray1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.button5);
+            this.splitContainer1.Panel1.Controls.Add(this.button4);
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(690, 540);
+            this.splitContainer1.SplitterDistance = 25;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "/";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(287, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(53, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "next";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(171, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 24);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "previous";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(557, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Font+";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(476, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Font-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(690, 511);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.FontChanged += new System.EventHandler(this.richTextBox1_FontChanged);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 540);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.richTextBoxArray1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private Microsoft.VisualBasic.Compatibility.VB6.RichTextBoxArray richTextBoxArray1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ PdfReader/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.PageSegmenter;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.ReadingOrderDetector;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.WordExtractor;
+using UglyToad.PdfPig;
+using System.Drawing;
+using System.Collections.Generic;
+using UglyToad.PdfPig.DocumentLayoutAnalysis;
+using UglyToad.PdfPig.Content;
+using System.Linq;
+using UglyToad.PdfPig.Graphics;
+using SixLabors.Fonts;
+using TextRenderer = System.Windows.Forms.TextRenderer;
+using Font = System.Drawing.Font;
+using FontStyle = System.Drawing.FontStyle;
+
+namespace PdfReader
+{
+    public partial class Form1 : Form
+    {
+        List<String> pages= new List<String>();
+        string pageText = "";
+        int page = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PDF files (*.pdf)|*.pdf|All files (*.*)|*.*";
+            pageText = "";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var document = PdfDocument.Open(openFileDialog.FileName);
+                foreach (Page page in document.GetPages())
+                {
+                 
+                    pageText+=page.Text;
+                    
+                }
+            }
+            richTextBox1.Font = new Font(richTextBox1.Font.FontFamily, richTextBox1.Font.Size, FontStyle.Regular);
+            FillPages(richTextBox1,pageText);
+        }
+
+        private static IReadOnlyList<TextBlock> GetTextBlocks(IEnumerable<Word> words, DocstrumBoundingBoxes pageSegmenter)
+        {
+            return pageSegmenter.GetBlocks(words);
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(richTextBox1.Font.FontFamily, richTextBox1.Font.Size - 1, FontStyle.Regular);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(richTextBox1.Font.FontFamily, richTextBox1.Font.Size +1 , FontStyle.Regular);
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(page-1<0)
+            {
+                return;
+            }
+            richTextBox1.Text = pages[--page];
+            label1.Text = page.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(page+1>pages.Count-1)
+            {
+                return;
+            }
+            richTextBox1.Text = pages[++page];
+            label1.Text = page.ToString();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_FontChanged(object sender, EventArgs e)
+        {
+            FillPages(richTextBox1,pageText);
+        }
+
+
+
+        private int CalculateCharsPerLine(RichTextBox richTextBox)
+        {
+            using (Graphics g = richTextBox.CreateGraphics())
+            {
+               
+                float charSize = richTextBox.Font.Size;
+                int charsPerLine = (int)(richTextBox.Width / (charSize/1.3333));
+                return charsPerLine;
+            }
+        }
+
+        private int CalculateLinesPerPage(RichTextBox richTextBox)
+        {
+            using (Graphics g = richTextBox.CreateGraphics())
+            {
+                SizeF charSize = g.MeasureString("a", richTextBox.Font);
+                int charsPerLine = (int)(richTextBox.Width / charSize.Width);
+                int linesPerPage = (int)(richTextBox.Height / charSize.Height);
+                return linesPerPage;
+            }
+        }
+
+
+        private void FillPages(RichTextBox richTextBox, string text)
+        {
+            pages.Clear();
+            int linesPerPage = CalculateLinesPerPage(richTextBox);
+            int charactersPerLine = CalculateCharsPerLine(richTextBox);
+
+            List<string> lines = new List<string>();
+            string[] words = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder currentLine = new StringBuilder();
+
+            foreach (string word in words)
+            {
+                if (currentLine.Length + word.Length + 1 <= charactersPerLine) // Учитываем пробел
+                {
+                    currentLine.Append(word);
+                    currentLine.Append(" ");
+                }
+                else
+                {
+                    lines.Add(currentLine.ToString().Trim());
+                    currentLine.Clear();
+                    currentLine.Append(word);
+                    currentLine.Append(" ");
+                }
+            }
+
+            if (currentLine.Length > 0)
+            {
+                lines.Add(currentLine.ToString().Trim());
+            }
+
+            int currentPage = 1;
+            int currentLines = 0;
+            StringBuilder currentPageText = new StringBuilder();
+            foreach (string line in lines)
+            {
+                if (currentLines < linesPerPage)
+                {
+                    currentPageText.AppendLine(line);
+                    currentLines++;
+                }
+                else
+                {
+                    pages.Add($"{Environment.NewLine}{currentPageText.ToString()}{Environment.NewLine}");
+                    currentPage++;
+                    currentPageText.Clear();
+                    currentPageText.AppendLine(line);
+                    currentLines = 1; // Учитываем первую строку новой страницы
+                }
+            }
+
+           
+            pages.Add($"{Environment.NewLine}{currentPageText.ToString()}{Environment.NewLine}");
+            if(pages.Count<=page)
+            {
+                page -= 1;
+            }
+            richTextBox1.Text = pages[page];
+            label1.Text = page.ToString();
+            label2.Text = "/";
+            label2.Text += currentPage-1;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if(pageText.Length > 0) {
+                FillPages(richTextBox1, pageText);
+            }
+            else
+            {
+                return;
+            }
+           
+        }
+    }
+}
